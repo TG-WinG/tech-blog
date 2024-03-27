7d5b5b449451ee7ac33391e0fd0b748e3e0b15bd
wingyou <winwing.you@gmail.com>
2024-03-28 01:47:03 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/kr/tgwing/tech/TechApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tgwing.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
