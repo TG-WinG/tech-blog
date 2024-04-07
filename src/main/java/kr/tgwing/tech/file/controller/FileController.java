97a1df2a6fed88b10fe54288f396a356bd98ae18
wingyou <winwing.you@gmail.com>
2024-04-07 21:42:01 +0900

    fix: multipart upload controller 에러 수정

--- src/main/java/kr/tgwing/tech/file/controller/FileController.java
+++ src/main/java/kr/tgwing/tech/file/controller/FileController.java
@@ -5,23 +5,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @Profile("dev")
-@RestController
 @RequiredArgsConstructor
 public class FileController {
 
     @Autowired
     FileServiceImpl fileServiceImpl;
     @PostMapping(path = "/file/image", consumes = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE})
-    public String uploadImage(
+    public ResponseEntity<String> uploadImage(
             @RequestPart(value = "name") String name,
             @RequestPart(value = "image") MultipartFile image
     ){
-        return fileServiceImpl.uploadImage(name, image);
+        return ResponseEntity.ok(fileServiceImpl.uploadImage(name, image));
     }
 }
