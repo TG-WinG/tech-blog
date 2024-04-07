9d42e1834d9f20a51d768c427f1eca7677ab6941
singsangssong <plmko0914@gmail.com>
2024-04-07 20:24:21 +0900

    [FEAT] jwt security 구성 완료

---
+++ src/main/java/kr/tgwing/tech/security/config/SecurityConfig.java
@@ -0,0 +1,92 @@
+package kr.tgwing.tech.security.config;
+
+
+import kr.tgwing.tech.security.filter.JwtFilter;
+import kr.tgwing.tech.security.util.JwtUtil;
+import kr.tgwing.tech.security.filter.LoginFilter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Log4j2
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("WebSecurity......................");
+
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean // 요청이 들어오면 SecurityFilterChain이 가로채서 인증, 인가를 체크함.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // 토큰 사용하기에 csrf 불가능
+                .cors(AbstractHttpConfigurer::disable)
+
+//                .formLogin(Customizer.withDefaults())// -> 로그인 화면 구성되면 사용해야함.
+//                .logout((logout) -> logout
+//                        .clearAuthentication(true))
+
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register", "/login")
+                        .permitAll()
+                        .requestMatchers("/admin/**")
+                        .hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+//    @Bean
+//    @ConditionalOnMissingBean(UserDetailsService.class)
+//    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        UserDetails admin = users
+//                .username("admin")
+//                .password("admin")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin);
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    } // 생성자 선언해둬야함... 해야되나?
+}
--- src/main/java/kr/tgwing/tech/user/service/UserServiceImpl.java
+++ src/main/java/kr/tgwing/tech/user/service/UserServiceImpl.java
@@ -2,17 +2,39 @@ package kr.tgwing.tech.user.service;
 
 import kr.tgwing.tech.user.dto.LoginDTO;
 import kr.tgwing.tech.user.dto.UserDTO;
+import kr.tgwing.tech.user.entity.UserEntity;
+import kr.tgwing.tech.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    @Override
-    public void register(UserDTO userDTO) {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Override
+    public void register(UserDTO userDTO) throws Exception{
+        String username = userDTO.getName();
+        String password = userDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByName(username);
+
+        if (isExist) {
+
+            return;
+        }
+
+        UserEntity data = UserDTO.toUserEntity(userDTO);
+
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_USER");
+
+        userRepository.save(data);
     }
+
 
     @Override
     public UserDTO login(LoginDTO loginDTO) {
