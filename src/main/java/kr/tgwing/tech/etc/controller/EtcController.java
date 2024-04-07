8d34570020418b42ea2b34f5a888e09abafb3d81
wingyou <winwing.you@gmail.com>
2024-04-07 17:53:26 +0900

    feat: 컨텍스트 패스 설정

--- src/main/java/kr/tgwing/tech/etc/controller/EtcController.java
+++ src/main/java/kr/tgwing/tech/etc/controller/EtcController.java
@@ -1,11 +1,9 @@
 package kr.tgwing.tech.etc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class EtcController {
     @GetMapping("/health")
     public String getHealth() {
--- src/main/java/kr/tgwing/tech/file/controller/FileController.java
+++ src/main/java/kr/tgwing/tech/file/controller/FileController.java
@@ -3,18 +3,14 @@ package kr.tgwing.tech.file.controller;
 import kr.tgwing.tech.file.service.FileServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
 public class FileController {
 
     @Autowired
