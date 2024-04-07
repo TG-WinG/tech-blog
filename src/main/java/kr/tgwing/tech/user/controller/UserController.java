395aff33d2ee760778e36b8d056c70edfd8dbb48
grapyju <yoonju203@naver.com>
2024-04-07 17:14:54 +0900

    feat: 반환값 제외 로그인

--- src/main/java/kr/tgwing/tech/user/controller/UserController.java
+++ src/main/java/kr/tgwing/tech/user/controller/UserController.java
@@ -1,15 +1,21 @@
 package kr.tgwing.tech.user.controller;
 
+import kr.tgwing.tech.user.dto.LoginDTO;
+import kr.tgwing.tech.user.dto.UserDTO;
+import kr.tgwing.tech.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping("/test")
     public ResponseEntity<String> test(Principal principal) {
 
@@ -17,4 +23,32 @@ public class UserController {
 
         return ResponseEntity.ok(name);
     }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<String> login(@RequestBody LoginDTO request) {
+        UserDTO login = userService.login(request);
+        //반환값?
+//        if(login != null){
+//            return ResponseEntity.ok().build(); //요거가 response다시
+//        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+    @PostMapping("/user/logout")
+    public ResponseEntity<String> logout(){
+        //
+        return ResponseEntity.ok("로그아웃되었습니다.");
+    }
+
+//    @PutMapping("/profile")
+//    public ResponseEntity<> changeProfile(@PathVariable Long id, @RequestBody profileDTO request){
+//        UserDTO change = userService.changeUser(id, request);
+//        if(change != null){
+//            return ResponseEntity.ok().build(); //요거가 response다시
+//        }
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//    }
+
 }
+
--- src/main/java/kr/tgwing/tech/user/service/UserServiceImpl.java
+++ src/main/java/kr/tgwing/tech/user/service/UserServiceImpl.java
@@ -2,12 +2,16 @@ package kr.tgwing.tech.user.service;
 
 import kr.tgwing.tech.user.dto.LoginDTO;
 import kr.tgwing.tech.user.dto.UserDTO;
+import kr.tgwing.tech.user.entity.UserEntity;
+import kr.tgwing.tech.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
 
     @Override
     public void register(UserDTO userDTO) {
@@ -17,9 +21,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO login(LoginDTO loginDTO) {
 
-        UserDTO userDTO = new UserDTO();
+        String studentId = loginDTO.getStudentId();
+        String password = loginDTO.getPassword();
 
-        return userDTO;
+        // UserRepository를 통해 데이터베이스에서 사용자 정보 조회
+        UserEntity userEntity = userRepository.findByStudentId(studentId);
+
+//        if (userEntity != null && userEntity.getPassword().equals(password)) {
+//            //성공했을 경우 반환값?
+//            return
+//        }
+
+        // 인증 실패 시 null 반환
+        return null;
     }
 
     @Override
