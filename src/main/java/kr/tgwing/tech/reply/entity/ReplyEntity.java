99c2b29b1a082fd57c55a6148420e301a608edb9
3un0ia <kj122kj@naver.com>
2024-04-30 21:25:03 +0900

    feat: 댓글 기능 구현

---
+++ src/main/java/kr/tgwing/tech/reply/entity/ReplyEntity.java
@@ -0,0 +1,45 @@
+package kr.tgwing.tech.reply.entity;
+
+import jakarta.persistence.*;
+import kr.tgwing.tech.common.BaseEntity;
+import kr.tgwing.tech.reply.dto.ReplyDto;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reply")
+public class ReplyEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JoinColumn(referencedColumnName = "id", table = "user", nullable = false)
+    private Long writer;
+
+    @Column(nullable = false)
+    private String description;
+
+    @JoinColumn(referencedColumnName = "id", table = "post", nullable = false)
+    private Long post;
+
+//    @ManyToOne
+//    @JoinColumn(name = "post")
+//    private PostEntity post;
+
+    public static ReplyDto toDto(ReplyEntity replyEntity) {
+        return ReplyDto.builder()
+                .id(replyEntity.id)
+                .writer(replyEntity.writer)
+                .description(replyEntity.description)
+                .modDate(replyEntity.getModDate())
+                .build();
+    }
+
+}
