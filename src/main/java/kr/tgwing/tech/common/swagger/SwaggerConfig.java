28d44b2e6c7da41ea844bf12a9e0be3976b396c6
wingyou <winwing.you@gmail.com>
2024-05-14 20:06:16 +0900

    fix: swagger url 수정

--- src/main/java/kr/tgwing/tech/common/swagger/SwaggerConfig.java
+++ src/main/java/kr/tgwing/tech/common/swagger/SwaggerConfig.java
@@ -6,30 +6,32 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
-@OpenAPIDefinition(
-        info = @Info(title = "tg-wing 홈페이지",
-                description = "다들 아자아자 화이팅^^",
-                version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "tg-wing 홈페이지", description = "다들 아자아자 화이팅^^", version = "v1"))
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
         SecurityScheme securityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
+        Server serverDev = new Server();
+        serverDev.setDescription("dev");
+        serverDev.setUrl("http://ec2-43-200-221-178.ap-northeast-2.compute.amazonaws.com/api");
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Arrays.asList(securityRequirement));
+                .security(Arrays.asList(securityRequirement))
+                .servers(List.of(serverDev));
     }
 
 }
-
