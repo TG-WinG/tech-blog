946bc1d88f6006362b970558197c5717d9a200c5
singsangssong <plmko0914@gmail.com>
2024-07-11 17:45:56 +0900

    refactor: 병합하기 전, 브랜치에서 미리 병합해봣음.

--- src/test/java/kr/tgwing/tech/user/LoginTest.java
+++ src/test/java/kr/tgwing/tech/user/LoginTest.java
@@ -17,27 +17,27 @@ public class LoginTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    @DisplayName("회원객체의 Date형식이 정확히 저장되는지를 확인하는 테스트")
-    public void registerTest() {
-
-        //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(5L);
-        userEntity.setStudentId("2020105622");
-        userEntity.setPassword("1234");
-        userEntity.setName("송성훈");
-        userEntity.setEmail("sonng@gmail.com");
-        userEntity.setBirth(Date.valueOf("2001-09-14"));
-        userEntity.setPhoneNumber("010-6348-5309");
-        userEntity.setRole("ROLE_USER");
-
-        //when
-        UserEntity result = userRepository.save(userEntity);
-
-        //then
-        Assertions.assertThat(userEntity.getBirth().getClass()).isEqualTo(result.getBirth().getClass());
-    }
+//    @Test
+//    @DisplayName("회원객체의 Date형식이 정확히 저장되는지를 확인하는 테스트")
+//    public void registerTest() {
+//
+//        //given
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setId(5L);
+//        userEntity.setStudentId("2020105622");
+//        userEntity.setPassword("1234");
+//        userEntity.setName("송성훈");
+//        userEntity.setEmail("sonng@gmail.com");
+//        userEntity.setBirth(Date.valueOf("2001-09-14"));
+//        userEntity.setPhoneNumber("010-6348-5309");
+//        userEntity.setRole("ROLE_USER");
+//
+//        //when
+//        UserEntity result = userRepository.save(userEntity);
+//
+//        //then
+//        Assertions.assertThat(userEntity.getBirth().getClass()).isEqualTo(result.getBirth().getClass());
+//    }
 
 
 }
