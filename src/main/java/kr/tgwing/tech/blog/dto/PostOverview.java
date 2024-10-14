4ada716bc6602debce8ea76dd227494ff6c53ffe
wingyou <winwing.you@gmail.com>
2024-10-14 23:01:45 +0900

    fix: missing field on post views

--- src/main/java/kr/tgwing/tech/blog/dto/PostOverview.java
+++ src/main/java/kr/tgwing/tech/blog/dto/PostOverview.java
@@ -1,5 +1,6 @@
 package kr.tgwing.tech.blog.dto;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 import lombok.Builder;
@@ -19,17 +20,22 @@ public class PostOverview {
     private ProfileDTO writer;
     private String title;
     private String summary;
+    private String thumbnail;
+    private String modDate;
     private int likeCount;
     private int commentCount;
     @Singular private final Set<String> hashtags;
 
     public static PostOverview of(Post post) {
         String content = post.getContent();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return PostOverview.builder()
                 .id(post.getId())
                 .writer(ProfileDTO.of(post.getWriter()))
                 .title(post.getTitle())
                 .summary((content.length() < SUMMARY_LENGTH) ? content : content.substring(0, SUMMARY_LENGTH))
+                .thumbnail(post.getThumbnail())
+                .modDate(post.getModDate().format(formatter))
                 .commentCount(post.getComments().size())
                 .hashtags(post.getHashtags().stream().map(hashtag -> hashtag.getName()).toList())
                 .build(); 
